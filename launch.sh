fefd35ec4e094bbedafb9f27d0fcd818e464f2ba
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-07-25 17:16:19 +0900

    Better handle host-dependent settings

--- launch.sh
+++ launch.sh
@@ -6,6 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
+# Host-dependent settings
+HOSTNAME=$(hostname)
+if [ "$HOSTNAME" == 'home' ]; then
+  export ETH_INTERFACE=enp8s0
+  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon1/temp2_input
+elif [ "$HOSTNAME" == 'dynamo' ]; then
+  export ETH_INTERFACE=enp67s0
+  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon0/temp2_input
+elif [ "$HOSTNAME" == 'forge' ]; then
+  export ETH_INTERFACE=enp0s31f6
+fi
+
 # Launch bar1 and bar2
 for m in $(xrandr --listmonitors | tail -n +2 | awk -e '{ print $4  }'); do
   MONITOR=$m polybar main&
