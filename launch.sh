b2fb580c5649ebf01d391951a0da3620a2b9f656
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-05-21 20:20:36 +0900

    Change temperature device files

--- launch.sh
+++ launch.sh
@@ -10,11 +10,11 @@ while pgrep -x polybar >/dev/null; do sleep 0.1; done
 HOSTNAME=$(cat /proc/sys/kernel/hostname)
 if [ "$HOSTNAME" == 'home' ]; then
   export ETH_INTERFACE=enp8s0
-  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon1/temp2_input
+  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon1/temp1_input
 elif [ "$HOSTNAME" == 'orion' ]; then
   export ETH_INTERFACE=enp86s0
   export WIFI_INTERFACE=wlp0s20f3
-  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon5/temp1_input
+  export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon4/temp1_input
 elif [ "$HOSTNAME" == 'dynamo' ]; then
   export ETH_INTERFACE=enp67s0
   export TEMPERATURE_HWMON_PATH=/sys/class/hwmon/hwmon1/temp2_input
